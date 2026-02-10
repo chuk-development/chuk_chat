36d8aa70622129e7f1f390a49afc5a9e2d6e5d74
hackerman <77645077+chukfinley@users.noreply.github.com>
2026-02-10 01:11:53 +0100

    chore: empty dart UI files for review

--- lib/platform_specific/root_wrapper.dart
+++ lib/platform_specific/root_wrapper.dart
@@ -1,5 +1,1 @@
-// lib/platform_specific/root_wrapper.dart
-// Central platform-adaptive wrapper that conditionally imports the correct implementation
-
-export 'root_wrapper_stub.dart'
-    if (dart.library.io) 'root_wrapper_io.dart';
+// placeholder
